2e9766544b65796923d3613bd1ee0c66b1349f53
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2026-01-28 13:42:59 -0300

    criação dos components cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Card } from "./card/card";
+import { Card2 } from "./card-2/card-2";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Card, Card2],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/card-2/card-2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-2',
+  imports: [],
+  templateUrl: './card-2.html',
+  styleUrl: './card-2.scss',
+})
+export class Card2 {
+
+}
