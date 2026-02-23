aa290bc74900ff1893ae8dc7c73ea21748d18005
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2026-02-23 14:05:49 -0300

    ajustes dos componentes e adicionando propriedades do angular material no slider

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CardButton } from '../card-button/card-button';
+import {MatSliderModule} from '@angular/material/slider';
 
 @Component({
   selector: 'app-card',
-  imports: [CardButton],
+  imports: [CardButton, MatSliderModule],
   templateUrl: './card.html',
   styleUrl: './card.scss',
+  //encapsulation: ViewEncapsulation.None
 })
 export class Card {
  plano = {
